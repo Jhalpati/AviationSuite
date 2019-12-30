bc187225d17bf123fd0b9c5e871914de8e261bb4
Jaykumar Halpati <rameshhalpati007@gmail.com>
2019-12-30 13:36:29 +0000

    Fixed spacing of Search button

--- src/ui.js
+++ src/ui.js
@@ -12,16 +12,21 @@ class UI extends React.Component {
     render() {
       return (
         <div>
+          <div>
             <form>
                 <TextField id="outlined-basic" label="Origin" variant="outlined"></TextField>
                 <span>&nbsp;&nbsp;</span>
                 <TextField id="outlined-basic" label="Destination" variant="outlined"></TextField>
             </form>
+              </div>
+              <span>&nbsp;&nbsp;</span>
 
 
+            <div>
             <Button variant="contained" color="primary" >
             Search
             </Button>
+            </div>
           
         </div>
       );
