5dab739edd41749ccce7a0bae894d2be5c5a9dec
Jaykumar Halpati <rameshhalpati007@gmail.com>
2020-05-31 12:05:56 +0100

    Added YACDN

--- src/components/news.js
+++ src/components/news.js
@@ -18,7 +18,7 @@ function News({ match }) {
     // fetch() is used to make a request to the API
     const fetchNews = await fetch(
       // Proxy was used to make this API call
-      `https://newsapi.org/v2/everything?q=${match.params.id}&apiKey=6e257f34188c4b608d5558758b35bf2f`
+      `https://yacdn.org/proxy/https://newsapi.org/v2/everything?q=${match.params.id}&apiKey=6e257f34188c4b608d5558758b35bf2f`
     );
 
     // Response received is stored as const item
